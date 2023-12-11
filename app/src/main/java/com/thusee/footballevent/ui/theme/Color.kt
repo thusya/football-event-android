8cc57b9077ceea8a48df9b499c2614323c8a568b
Thuseeman <thushfit@gmail.com>
2023-12-11 19:36:01 +0000

    Initial commit

---
+++ app/src/main/java/com/thusee/footballevent/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.thusee.footballevent.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DeepGreen = Color(0xFF115740)
+val RichMaroon = Color(0xFF800000)
+val Pink80 = Color(0xFFEFB8C8)
+val BackgroundDark = Color(0xFF121212)
+
+val BrightGreen = Color(0xFF60A561)
+val SoftRed = Color(0xFFE57373)
+val Pink40 = Color(0xFF7D5260)
+val BackgroundLight = Color(0xFFF5F5F5)
---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.version.catalog.update)
+    alias(libs.plugins.versions.update)
+}
+true // Needed to make the Suppress annotation work for the plugins block
